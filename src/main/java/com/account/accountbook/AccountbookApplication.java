dc694d141d0e924c7530c0dff076be25c0dd883b
mjey <mjey54@naver.com>
2024-06-10 15:34:22 +0900

    db 설정

--- src/main/java/com/account/accountbook/AccountbookApplication.java
+++ src/main/java/com/account/accountbook/AccountbookApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication()
 public class AccountbookApplication {
 
 	public static void main(String[] args) {
